8faa4f938af432db6ab31ac30184e57fd6b1e64a
donov <donovann.dev@gmail.com>
2023-01-18 18:53:34 +0100

    [Fix] Correction Authentification

--- src/functions/login.js
+++ src/functions/login.js
@@ -1,29 +1,52 @@
-export async function registerUser(user, callback) {
-  const response = await fetch(process.env.VUE_APP_API_URL + "/login/signup", {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(user)
-  });
+import router from "@/router";
 
-  const data = await response.json();
-  callback(data);
+export async function registerUser(user, callback) {
+   const response = await fetch(process.env.VUE_APP_API_URL + "/login/signup", {
+      method: "POST",
+      mode: "cors",
+      credentials: 'include',
+      headers: {
+         "Accept": "application/json",
+         "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+   });
+
+   const data = await response.json();
+   callback(data);
 }
 
 export async function logUser(user, callback) {
-  const response = await fetch(process.env.VUE_APP_API_URL + "/login/signin", {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(user)
-  });
+   const response = await fetch(process.env.VUE_APP_API_URL + "/login/signin", {
+      method: "POST",
+      mode: "cors",
+      credentials: 'include',
+      headers: {
+         "Accept": "application/json",
+         "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+   });
 
-  const data = await response.json();
-  callback(data);
+   const data = await response.json();
+   callback(data);
+}
+
+export async function checkUserLogged(callback) {
+   const response = await fetch(process.env.VUE_APP_API_URL + "/login/logged", {
+      method: "GET",
+      credentials: "include",
+      mode: "cors",
+      headers: {
+         "Accept": "application/json",
+         "Access-Control-Allow-Credentials": "true",
+      },
+   });
+
+   const data = await response.json();
+   if (data.firstname && data.surname && data.type) {
+      callback(data);
+   } else {
+      await router.push("/signin")
+   }
 }
