9dc0be3df14913c6627298b9db6bb9536db339d5
donov <donovann.dev@gmail.com>
2023-01-20 21:01:19 +0100

    [Fix] Correction background body

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,15 @@
 </template>
 
 <style>
+body {
+   background: #eeebf3;
+}
+
 #app {
    font-family: Avenir, Helvetica, Arial, sans-serif;
    -webkit-font-smoothing: antialiased;
    -moz-osx-font-smoothing: grayscale;
    color: #2c3e50;
-   background-color: #eeebf3;
 }
 
 nav {
