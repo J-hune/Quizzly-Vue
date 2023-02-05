1db15083e1cd5811ac6fc1b1fa43c3a64161a535
donov <donovann.dev@gmail.com>
2023-02-05 14:27:32 +0100

    [Refact] Modification fonctionnement composant uniqueResponse

--- src/components/Questions/uniqueResponse.vue
+++ src/components/Questions/uniqueResponse.vue
@@ -1,8 +1,7 @@
 <template>
    <h2 class="text-xl font-medium text-gray-900">Votre réponse :</h2>
 
-   <input type="number" @input="updateResponseValue($event.target.value)" :value="modelValue"
-          @keydown="onlyNumbers($event)"
+   <input type="number" v-model="inputVal" @keydown="onlyNumbers($event)"
           class="mt-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300
                    focus:ring-indigo-200 focus:border-indigo-500 focus:ring-2 outline-none
                    text-gray-900 py-1 px-3 leading-8 transition-colors duration-150 ease-in-out"
@@ -13,11 +12,17 @@
 export default {
    name: "uniqueResponse",
    props: { modelValue: String },
-   emits: ["update:modelValue"],
+   computed: {
+      inputVal: {
+         get() {
+            return this.modelValue;
+         },
+         set(val) {
+            this.$emit("update:modelValue", val.toString());
+         }
+      }
+   },
    methods: {
-      updateResponseValue(newValue) {
-         this.$emit("update:modelValue", newValue);
-      },
       onlyNumbers(e) {
          // Pour éviter les "e" dans le champ
          if (e.keyCode === 69) e.preventDefault();
