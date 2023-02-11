0e6ad5a00b6429546e87c36945d4717c3b9be1ea
donov <donovann.dev@gmail.com>
2023-02-11 17:14:00 +0100

    [Edit] Modification font

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ body {
 }
 
 #app {
-   font-family: Avenir, Helvetica, Arial, sans-serif;
+   font-family: Arial, sans-serif;
    -webkit-font-smoothing: antialiased;
    -moz-osx-font-smoothing: grayscale;
    color: #2c3e50;
