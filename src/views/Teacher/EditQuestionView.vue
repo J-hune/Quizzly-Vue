af2c340ecc2bf68fcb07de6928b8c58e06b02f05
donov <donovann.dev@gmail.com>
2023-03-07 12:04:39 +0100

    [Refact] Modification de la vue EditQuestion
    - Suppression des toRaw
    - Modification de l'objet labels (tableau => dictionnaire)

--- src/views/Teacher/EditQuestionView.vue
+++ src/views/Teacher/EditQuestionView.vue
@@ -33,7 +33,7 @@
 
             <!-- Génération des étiquettes avec un v-bind for -->
             <div class="custom-flex">
-               <a v-for="label in question.etiquettes" :key="label.nom" @click="removeLabel(label.nom)"
+               <a v-for="label in question.etiquettes" :key="label.id" @click="removeLabel(label.id)"
                   class="px-4 py-0.5 rounded-md cursor-no-drop border-2"
                   :style="{backgroundColor: `#${label.couleur}`, borderColor: `#${label.couleur}`, color: getContrast(`#${label.couleur}`)}">
                   {{ label.nom }}</a>
@@ -69,7 +69,7 @@
       </button>
 
       <button
-          class="relative mt-5 ml-5 sm:ml-6 mr-3 mb-5 bg-red-500 hover:bg-red-600 text-white
+        class="relative mt-5 ml-5 sm:ml-6 mr-3 mb-5 bg-red-500 hover:bg-red-600 text-white
         font-bold py-2 px-7 rounded-lg" @click="remove">
          Supprimer la Question
       </button>
@@ -77,14 +77,13 @@
 </template>
 
 <script>
-import { toRaw } from "vue";
 import { useRoute } from "vue-router";
 import mermaid from "mermaid";
 import { useToast } from "vue-toastification";
 
 import { fetchData } from "@/functions/fetch";
 import { TextToHtml } from "@/functions/textTohtml";
-import {editQuestion, removeQuestion} from "@/functions/questions";
+import { editQuestion, removeQuestion } from "@/functions/questions";
 
 import RedirectBack from "@/components/redirectBack";
 import ModalComponent from "@/components/ModalComponent";
@@ -102,7 +101,7 @@ export default {
          question: Object,
          labels: [],
          show: false,
-         html: [],
+         html: []
       };
    },
    mounted() {
@@ -111,6 +110,13 @@ export default {
    setup() {
       const toast = useToast();
       return { toast };
+   },
+   async created() {
+      const route = useRoute();
+      const { data } = await fetchData("/questions/getQuestion/" + route.params.id);
+      const { data: allLabels } = await fetchData("/labels/getAllLabels");
+      this.labels = allLabels;
+      this.question = data;
    },
    methods: {
       getContrast: function(hexcolor) {
@@ -123,23 +129,19 @@ export default {
          return (yiq >= 128) ? "black" : "white";
       },
       addLabel: function(label) {
-
          // Verification doublons et push
-         let labels = toRaw(this.question.etiquettes);
-         if (!labels.find(e => e.nom === label[0])) {
-            this.question.etiquettes.push({ couleur: label[1], nom: label[0] });
+         if (!this.question.etiquettes.find(e => e.id === label.id)) {
+            this.question.etiquettes.push(label);
          }
 
          this.show = false;
       },
       removeLabel: function(label) {
-
-         // On supprime label de this.question.etiquettes
-         const labels = toRaw(this.question.etiquettes);
-         this.question.etiquettes = labels.filter(e => e.nom !== label);
+         // On supprime l'étiquette de this.question.etiquettes
+         this.question.etiquettes = this.question.etiquettes.filter(e => e.id !== label);
       },
       canSave: function() {
-         const Question = toRaw(this.question);
+         const Question = this.question;
          const regex = /^[-+]?\d+(\.\d{0,2}|)$/;
 
          if (!Question.enonce || !Question.etiquettes.length) return false;
@@ -149,57 +151,46 @@ export default {
          return true;
       },
       save: function() {
-         const question = toRaw(this.question);
-         editQuestion(question, (data) => {
+         editQuestion(this.question, (data) => {
             if (data.success) this.toast.success("La question a été modifiée");
             else this.toast.error("Une erreur a eu lieu lors de la modification de la question");
          });
       },
-      remove: function () {
-         const question = toRaw(this.question);
-         removeQuestion(question, (data) => {
+      remove: function() {
+         removeQuestion(this.question, (data) => {
             if (data.success) {
                this.toast.success("La question a été supprimée");
-               router.back()
-            }
-            else this.toast.error("Une erreur a eu lieu lors de la suppression de la question");
+               router.back();
+            } else this.toast.error("Une erreur a eu lieu lors de la suppression de la question");
          });
       },
       onChildUpdate: function(newValue) {
-         this.question.type = !newValue ? 0 : 1
+         this.question.type = !newValue ? 0 : 1;
       }
    },
    watch: {
       "question": {
          // On met en place un watcher sur question pour ajouter ou supprimer des réponses
          handler: function(newQuestion) {
-            const question = toRaw(newQuestion);
 
             // On met à jour html
-            this.html = TextToHtml(question.enonce);
+            this.html = TextToHtml(newQuestion.enonce);
 
             // Si aucun contenu de réponse n'est vide
-            if (!question?.reponses.find(e => !e.reponse.trim())) {
+            if (!newQuestion?.reponses.find(e => !e.reponse.trim())) {
                this.question.reponses.push(
                  { "reponse": "", "reponseJuste": false }
                );
             } else {
                //S'il y a plus d'une réponse sans contenu
-               if (question?.reponses.filter(e => !e.reponse.trim()).length > 1) {
-                  let emptyRes = question.reponses.findIndex(e => !e.reponse);
+               if (newQuestion?.reponses.filter(e => !e.reponse.trim()).length > 1) {
+                  let emptyRes = newQuestion.reponses.findIndex(e => !e.reponse);
                   this.question.reponses.splice(emptyRes, 1);
                }
             }
          },
          deep: true
       }
-   },
-   async created() {
-      const route = useRoute();
-      const { data } = await fetchData("/questions/getQuestion/" + route.params.id);
-      const { data: allLabels } = await fetchData("/labels/getAllLabels");
-      this.labels = allLabels;
-      this.question = data;
    }
 };
 </script>
