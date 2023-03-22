be1c43ed2afa0e6448f367411b57c94aacd58d4d
donov <donovann.dev@gmail.com>
2023-03-22 20:33:40 +0100

    [Edit] Modification marge page "Mes Etudiants"

--- src/views/Teacher/LabelsList.vue
+++ src/views/Teacher/LabelsList.vue
@@ -3,7 +3,7 @@
    <div class="px-7 md:px-16 py-12 w-full">
 
       <!-- Titre et explications de la page -->
-      <div class="flex flex-col w-full py-1 mb-6">
+      <div class="flex flex-col w-full py-1 mb-12">
          <h1 class="sm:text-3xl text-2xl font-semibold title-font mb-4 text-gray-900">Mes Etiquettes</h1>
          <p class="leading-relaxed text-base">Les étiquettes que vous avez créées ou associées à vos
             questions sont affichées ci-dessous.<br>
