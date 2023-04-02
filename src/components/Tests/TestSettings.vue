66a3fc3b83b0ee8bf6eb7e9b23b4a1b93541bc7d
donov <donovann.dev@gmail.com>
2023-04-02 22:40:08 +0200

    [Fix] Génération des Evaluations: Ajout du fetch des données

--- src/components/Tests/TestSettings.vue
+++ src/components/Tests/TestSettings.vue
@@ -1,100 +1,100 @@
 <template>
-   <div class="mt-6">
+  <div class="mt-6">
 
-      <!-- Pour chaque etiquette sélectionnée -->
-      <div class="label-container mb-5 sm:w-3/4" v-for="label in labels" :key="label.id">
+    <!-- Pour chaque etiquette sélectionnée -->
+    <div class="label-container mb-5 sm:w-3/4" v-for="label in labels" :key="label.id">
 
-         <!-- Etiquette avec nom et couleur -->
-         <a @click="removeLabel(label.id)"
-            class="px-4 py-0.5 rounded-md cursor-no-drop border-2 whitespace-nowrap"
-            :style="{backgroundColor: `#${label.couleur}`, borderColor: `#${label.couleur}`, color: getContrast(`#${label.couleur}`)}">
-            {{ label.nom }}
-         </a>
-
-         <!-- Input type range avec min et max calculés -->
-         <slider class="ml-8 sm:ml-16 w-full slider-quizzly" :min="label.range.min" :max="label.range.max"
-                 v-model="label.range.value" :step="1" />
-      </div>
-
-      <!-- Bouton pour ajouter de nouvelles étiquettes -->
-      <a class="px-4 py-1 rounded-md add-label cursor-pointer select-none" @click="show = true">
-         Ajouter une étiquette au sujet
+      <!-- Etiquette avec nom et couleur -->
+      <a @click="removeLabel(label.id)"
+         class="px-4 py-0.5 rounded-md cursor-no-drop border-2 whitespace-nowrap"
+         :style="{backgroundColor: `#${label.couleur}`, borderColor: `#${label.couleur}`, color: getContrast(`#${label.couleur}`)}">
+        {{ label.nom }}
       </a>
 
+      <!-- Input type range avec min et max calculés -->
+      <slider class="ml-8 sm:ml-16 w-full slider-quizzly" :min="label.range.min" :max="label.range.max"
+              v-model="label.range.value" :step="1" />
+    </div>
 
-      <hr class="my-8">
+    <!-- Bouton pour ajouter de nouvelles étiquettes -->
+    <a class="px-4 py-1 rounded-md add-label cursor-pointer select-none" @click="show = true">
+      Ajouter une étiquette au sujet
+    </a>
 
-      <!-- Paramètres du sujet -->
-      <div class="2xl:flex lg:gap-8">
-         <div>
-            <!-- Paramètre: Mélange des questions dans le sujet -->
-            <div class="flex flex-row mb-2">
-               <div class="self-center mr-3">
-                  <label class="relative inline-flex align-middle cursor-pointer">
-                     <input type="checkbox" value="" class="sr-only peer outline-none"
-                            v-model="groupQuestions">
-                     <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-['']
+
+    <hr class="my-8">
+
+    <!-- Paramètres du sujet -->
+    <div class="2xl:flex lg:gap-8">
+      <div>
+        <!-- Paramètre: Mélange des questions dans le sujet -->
+        <div class="flex flex-row mb-2">
+          <div class="self-center mr-3">
+            <label class="relative inline-flex align-middle cursor-pointer">
+              <input type="checkbox" value="" class="sr-only peer outline-none"
+                     v-model="groupQuestions">
+              <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-['']
                              after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5
                              after:transition-all peer-checked:bg-blue-600" />
-                  </label>
-               </div>
+            </label>
+          </div>
 
-               <h2 class="text-lg">Les questions sont regroupées par étiquettes</h2>
-            </div>
+          <h2 class="text-lg">Les questions sont regroupées par étiquettes</h2>
+        </div>
 
-            <!-- Paramètre: Conservation de l'ordre des étiquettes -->
-            <div v-if="groupQuestions" class="flex flex-row mb-2">
-               <div class="self-center mr-3">
-                  <label class="relative inline-flex align-middle cursor-pointer">
-                     <input type="checkbox" value="" class="sr-only peer outline-none"
-                            v-model="keepLabelsOrder">
-                     <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-['']
+        <!-- Paramètre: Conservation de l'ordre des étiquettes -->
+        <div v-if="groupQuestions" class="flex flex-row mb-2">
+          <div class="self-center mr-3">
+            <label class="relative inline-flex align-middle cursor-pointer">
+              <input type="checkbox" value="" class="sr-only peer outline-none"
+                     v-model="keepLabelsOrder">
+              <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-['']
                              after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5
                              after:transition-all peer-checked:bg-blue-600" />
-                  </label>
-               </div>
+            </label>
+          </div>
 
-               <h2 class="text-lg">L'ordre des étiquettes est conservé</h2>
-            </div>
-         </div>
-
-
-         <hr class="mt-8 mb-8">
-
-         <div>
-            <!-- Paramètre: Nombre de sujets à créer -->
-            <div class="sm:flex mb-4">
-
-               <h2 class="mb-2 mr-0 sm:mr-4 sm:mb-0 text-lg">Nombre de sujets à générer :</h2>
-               <input type="number" :value="size" @input="updateSize"
-                      class="w-full sm:w-fit text-gray-700 bg-gray-50 rounded-lg border border-gray-300
-                   focus:ring-indigo-200 focus:border-indigo-200 focus:ring-2 outline-none
-                   px-3 leading-8 transition-colors duration-150 ease-in-out"
-                      placeholder="Nombre de sujets..." />
-            </div>
-
-
-            <!-- Paramètre: Nombre de questions dans chaque sujet -->
-            <div class="sm:flex mb-6">
-               <h2 class="mb-2 mr-0 sm:mr-4 sm:mb-0 text-lg">Nombre de questions dans le sujet
-                  :</h2>
-               <input type="number" :value="questionSize" @input="updateQuestionSize"
-                      class="w-full sm:w-fit text-gray-700 bg-gray-50 rounded-lg border border-gray-300
-                   focus:ring-indigo-200 focus:border-indigo-200 focus:ring-2 outline-none
-                   px-3 leading-8 transition-colors duration-150 ease-in-out"
-                      placeholder="Nombre de questions..." />
-            </div>
-
-         </div>
+          <h2 class="text-lg">L'ordre des étiquettes est conservé</h2>
+        </div>
       </div>
 
-      <!-- "Popup" Modal permettant de selectionner et de créer des étiquettes -->
-      <modal-component v-model="show">
-         <template v-slot:content>
-            <add-labels @add-label="addLabel" />
-         </template>
-      </modal-component>
-   </div>
+
+      <hr class="mt-8 mb-8">
+
+      <div>
+        <!-- Paramètre: Nombre de sujets à créer -->
+        <div class="sm:flex mb-4">
+
+          <h2 class="mb-2 mr-0 sm:mr-4 sm:mb-0 text-lg">Nombre de sujets à générer :</h2>
+          <input type="number" :value="size" @input="updateSize"
+                 class="w-full sm:w-fit text-gray-700 bg-gray-50 rounded-lg border border-gray-300
+                   focus:ring-indigo-200 focus:border-indigo-200 focus:ring-2 outline-none
+                   px-3 leading-8 transition-colors duration-150 ease-in-out"
+                 placeholder="Nombre de sujets..." />
+        </div>
+
+
+        <!-- Paramètre: Nombre de questions dans chaque sujet -->
+        <div class="sm:flex mb-6">
+          <h2 class="mb-2 mr-0 sm:mr-4 sm:mb-0 text-lg">Nombre de questions dans le sujet
+            :</h2>
+          <input type="number" :value="questionSize" @input="updateQuestionSize"
+                 class="w-full sm:w-fit text-gray-700 bg-gray-50 rounded-lg border border-gray-300
+                   focus:ring-indigo-200 focus:border-indigo-200 focus:ring-2 outline-none
+                   px-3 leading-8 transition-colors duration-150 ease-in-out"
+                 placeholder="Nombre de questions..." />
+        </div>
+
+      </div>
+    </div>
+
+    <!-- "Popup" Modal permettant de selectionner et de créer des étiquettes -->
+    <modal-component v-model="show">
+      <template v-slot:content>
+        <add-labels @add-label="addLabel" />
+      </template>
+    </modal-component>
+  </div>
 </template>
 
 <script>
@@ -107,7 +107,7 @@ import Slider from "@vueform/slider";
 export default {
    name: "TestSettings",
    components: { ModalComponent, AddLabels, Slider },
-   emits: ["updateLabels", "updateSize", "updateQuestionSize"],
+   emits: ["updateLabels", "updateSize", "updateQuestionSize", "updateLabelsOrderSetting", "updateGroupQuestionsSetting"],
    props: { labels: Array, size: Number, questions: Array, questionSize: Number },
    data: function() {
       return {
@@ -170,6 +170,15 @@ export default {
       updateQuestionSize: function(event) {
          this.$emit("updateQuestionSize", parseInt(event.target.value));
       }
+   },
+   watch: {
+      keepLabelsOrder: function(newValue) {
+         this.$emit("updateLabelsOrderSetting", newValue);
+      },
+
+      groupQuestions: function(newValue) {
+         this.$emit("updateGroupQuestionsSetting", newValue);
+      }
    }
 };
 </script>
@@ -177,20 +186,20 @@ export default {
 <style src="@vueform/slider/themes/default.css"></style>
 <style scoped>
 .add-label {
-   border: solid 2px #a56cc1;
+    border: solid 2px #a56cc1;
 }
 
 .label-container {
-   border: solid 2px #d0c8dd;
-   padding: 36px 26px 12px 12px;
-   border-radius: 10px;
-   display: flex;
-   align-items: baseline;
+    border: solid 2px #d0c8dd;
+    padding: 36px 26px 12px 12px;
+    border-radius: 10px;
+    display: flex;
+    align-items: baseline;
 }
 
 .slider-quizzly {
-   --slider-connect-bg: #A6ACEC;
-   --slider-tooltip-bg: #A56CC1;
-   --slider-handle-ring-color: rgba(165, 108, 193, 0.10);
+    --slider-connect-bg: #A6ACEC;
+    --slider-tooltip-bg: #A56CC1;
+    --slider-handle-ring-color: rgba(165, 108, 193, 0.10);
 }
 </style>
--- src/views/Teacher/NewTestView.vue
+++ src/views/Teacher/NewTestView.vue
@@ -1,65 +1,67 @@
 <template>
    <!-- Block Principal -->
-   <div class="page-print px-7 md:px-16 pt-10 w-full">
-      <div class="mb-6">
+  <div class="page-print px-7 md:px-16 pt-10 w-full">
+    <div class="mb-6">
 
-         <!-- Titre de la page -->
-         <div class="flex flex-col w-full mb-10 py-1">
-            <h1 class="sm:text-3xl text-2xl font-semibold title-font mb-4 text-gray-900">Créer une évaluation</h1>
-            <p class="leading-relaxed text-base">Il existe deux types d'évaluations que vous pouvez créer : les quiz et
-               les contrôles.<br>
-               Les quiz sont des listes de questions que vous pouvez choisir et organiser dans l'ordre que vous
-               souhaitez.<br>
-               Les contrôles, quant à eux, sont générés de manière aléatoire en fonction des paramètres que vous
-               choisissez.</p>
-         </div>
-
-
-         <!-- Titre du QCM -->
-         <h2 class="mb-2 text-xl font-medium text-gray-900">Titre du Sujet :</h2>
-         <input type="text" v-model="title" @keyup.enter="print"
-                class="w-full text-gray-700 bg-gray-50 rounded-lg border border-gray-300
-                   focus:ring-indigo-200 focus:border-indigo-200 focus:ring-2 outline-none
-                   py-1 px-3 leading-8 transition-colors duration-150 ease-in-out"
-                placeholder="Écrivez le titre du Sujet..." />
+      <!-- Titre de la page -->
+      <div class="flex flex-col w-full mb-10 py-1">
+        <h1 class="sm:text-3xl text-2xl font-semibold title-font mb-4 text-gray-900">Créer une évaluation</h1>
+        <p class="leading-relaxed text-base">Il existe deux types d'évaluations que vous pouvez créer : les quiz et
+          les contrôles.<br>
+          Les quiz sont des listes de questions que vous pouvez choisir et organiser dans l'ordre que vous
+          souhaitez.<br>
+          Les contrôles, quant à eux, sont générés de manière aléatoire en fonction des paramètres que vous
+          choisissez.</p>
       </div>
 
-      <hr class="mt-7 mb-8" />
 
-      <!-- On montre tous les types d'évaluations disponibles (pour l'instant QCM et Contrôle -->
-      <switch-button class="mb-4" :response-type="testType"
-                     :types="[{id: 0, name:'Sujet de QCM'}, {id: 1, name:'Sujet de Contrôle'}, {id: 2, name:'Sujet de Contrôle Anonyme'} ]"
-                     @update="(type) => testType = type" />
+      <!-- Titre du QCM -->
+      <h2 class="mb-2 text-xl font-medium text-gray-900">Titre du Sujet :</h2>
+      <input type="text" v-model="title" @keyup.enter="print"
+             class="w-full text-gray-700 bg-gray-50 rounded-lg border border-gray-300
+                   focus:ring-indigo-200 focus:border-indigo-200 focus:ring-2 outline-none
+                   py-1 px-3 leading-8 transition-colors duration-150 ease-in-out"
+             placeholder="Écrivez le titre du Sujet..." />
+    </div>
 
-      <!-- Dans le cas où c'est un QCM -->
-      <p v-if="testType === 0" class="mb-6">Les questions que vous avez déjà créées sont affichées sur la
-         <span class="cursor-pointer" @click="openLink('gauche')">gauche.</span><br>
-         Faites les glisser sur la
-         <span class="cursor-pointer" @click="openLink('droite')">droite</span>
-         pour les ajouter au sujet.</p>
-      <draggable-questions v-if="testType === 0" :selected-questions="selectedQuestions"
-                           :all-questions="allQuestions" />
+    <hr class="mt-7 mb-8" />
 
-      <!-- Dans le cas où c'est un Contrôle -->
-      <test-settings v-else class="mt-6 mb-10" :labels="testLabels" :questions="allQuestions"
-                     @updateLabels="(labels) => this.testLabels = labels" :size="testSize"
-                     @updateSize="(size) => this.testSize = size"
-                     @update-question-size="size => this.testQuestionSize = size" :question-size="testQuestionSize" />
-   </div>
+    <!-- On montre tous les types d'évaluations disponibles (pour l'instant QCM et Contrôle) -->
+    <switch-button class="mb-4" :response-type="testType"
+                   :types="[{id: 0, name:'Sujet de QCM'}, {id: 1, name:'Sujet de Contrôle'}, {id: 2, name:'Sujet de Contrôle Anonyme'} ]"
+                   @update="(type) => testType = type" />
+
+    <!-- Dans le cas où c'est un QCM -->
+    <p v-if="testType === 0" class="mb-6">Les questions que vous avez déjà créées sont affichées sur la
+      <span class="cursor-pointer" @click="openLink('gauche')">gauche.</span><br>
+      Faites les glisser sur la
+      <span class="cursor-pointer" @click="openLink('droite')">droite</span>
+      pour les ajouter au sujet.</p>
+    <draggable-questions v-if="testType === 0" :selected-questions="selectedQuestions"
+                         :all-questions="allQuestions" />
+
+    <!-- Dans le cas où c'est un Contrôle -->
+    <test-settings v-else class="mt-6 mb-10" :labels="testLabels" :questions="allQuestions"
+                   @updateLabels="(labels) => this.testLabels = labels" :size="testSize"
+                   @updateSize="(size) => this.testSize = size"
+                   @update-question-size="size => this.testQuestionSize = size" :question-size="testQuestionSize"
+                   @update-group-questions-setting="setting => this.groupQuestions = setting"
+                   @update-labels-order-setting="setting => this.keepLabelsOrder = setting" />
+  </div>
 
    <!-- Bouton Impression -->
-   <div class="save">
-      <button
-        class="relative ml-auto mt-5 mr-6 mb-5 bg-blue-500 hover:bg-blue-600 text-white
+  <div class="save">
+    <button
+      class="relative ml-auto mt-5 mr-6 mb-5 bg-blue-500 hover:bg-blue-600 text-white
         font-bold py-2 px-7 rounded-lg disabled:opacity-40 right-0" @click="print"
-        :disabled="!canSave()">
-         Imprimer l'évaluation
-      </button>
-   </div>
+      :disabled="!canSave()">
+      Imprimer l'évaluation
+    </button>
+  </div>
 
    <!-- "Popup" qui apparait uniquement si on print -->
-   <render-qcm v-if="testType === 0" :title="renderTitle" :questions="renderQuestions" />
-   <render-test v-else :type="testType" :title="renderTitle" :tests="renderTests" />
+  <render-qcm v-if="testType === 0" :title="renderTitle" :questions="renderQuestions" />
+  <render-test v-else :type="testType" :title="renderTitle" :tests="renderTests" />
 </template>
 
 <script>
@@ -68,7 +70,7 @@ import { fetchData } from "@/functions/fetch";
 import RenderQcm from "@/components/Tests/RenderQcm.vue";
 import SwitchButton from "@/components/SwitchButton.vue";
 import TestSettings from "@/components/Tests/TestSettings.vue";
-import { nextTick, toRaw } from "vue";
+import { nextTick } from "vue";
 import RenderTest from "@/components/Tests/RenderTest.vue";
 import { useToast } from "vue-toastification";
 
@@ -90,29 +92,44 @@ export default {
          allQuestions: [],
          renderTitle: "",
          renderQuestions: [],
-         renderTests: []
+         renderTests: [],
+         keepLabelsOrder: false,
+         groupQuestions: true
       };
    },
    methods: {
       /**
        * Affiche les questions sélectionnées et le titre dans la vue d'impression.
        */
-      print: function() {
+      print: async function() {
          if (this.testType === 0) {
             this.renderQuestions = this.selectedQuestions;
             this.renderTitle = this.title;
 
             // On attend le tick suivant pour que renderQuestions et renderTitle = soient à jour
-            nextTick(() => {
-               window.print();
-            });
+            await nextTick();
+            window.print();
          } else {
-            //TODO supprimer les trois lignes suivantes pour ajouter le fetch
-            console.log({ size: this.testSize, labels: toRaw(this.testLabels) });
+            const response = await fetch(process.env.VUE_APP_API_URL + "/api/evaluations/generateEvaluations", {
+               method: "POST",
+               mode: "cors",
+               credentials: "include",
+               headers: {
+                  "Accept": "application/json",
+                  "Content-Type": "application/json"
+               },
+               body: JSON.stringify({
+                  size: this.testSize,
+                  questionSize: this.testQuestionSize,
+                  groupQuestions: this.groupQuestions,
+                  keepLabelsOrder: this.groupQuestions ? this.keepLabelsOrder : false,
+                  labels: this.testLabels
+               })
+            });
 
-            const result = [[6, 18, 21, 61, 62], [21, 61, 62, 6, 18], [61, 62], [62, 21, 6, 18, 61]];
+            const data = await response.json();
 
-            this.renderTests = result.map(e => e.map(f => this.allQuestions.find(g => g.id === f)))
+            this.renderTests = data.map(e => e.map(f => this.allQuestions.find(g => g.id === f)));
             this.renderTitle = this.title;
 
             const rangeQuestions = this.testLabels.reduce((accumulator, currentValue) => {
@@ -173,26 +190,26 @@ export default {
 
 <style scoped>
 @media print {
-   .page-print {
-      display: none;
-   }
+    .page-print {
+        display: none;
+    }
 }
 </style>
 
 <style>
 .markdown-body {
-   padding: 0 30px 5px 30px;
+    padding: 0 30px 5px 30px;
 }
 
 @media (max-width: 1280px) {
-   .markdown-body {
-      min-height: 72px;
-   }
+    .markdown-body {
+        min-height: 72px;
+    }
 }
 
 .save {
-   width: 100%;
-   display: inline-flex;
-   border-top: solid 1px #eaeaea;
+    width: 100%;
+    display: inline-flex;
+    border-top: solid 1px #eaeaea;
 }
 </style>
